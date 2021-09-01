81c90d6376f740d810f698c98ed9c19cef778e60
BlazeATG <ubietaalex@gmail.com>
2021-09-01 10:30:07 -0500

    Discord req

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { Client, Intents } = require('discord.js');
+const Discord = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const chrono = require('chrono-node');
 const { prefix, token } = require('./config.json');
